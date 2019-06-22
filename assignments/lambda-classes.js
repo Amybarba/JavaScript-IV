0db8a99c9840136631a4c7fa7e9addbdec5ed66c
Amy Barba <Amybarba70@gmail.com>
2019-06-22 16:18:26 -0700

    cleanup and correct code, add refactoring to ES6 classes

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,4 +1,4 @@
-// CODE here for your Lambda Classes
+// // CODE here for your Lambda Classes
 class Person {
     constructor(props) {
         this.name = props.name;
@@ -24,21 +24,19 @@ class Instructor extends Person {
     }
 
     grade(student,subject) {
-        console.log(`${student.name}
-        recieves a perfect score on ${subject}`);
+        console.log(`${student.name} recieves a perfect score on ${subject}`);
     }
 }
 
 class Student extends Person {
     constructor(props) {
         super(props);
-        this.previousBackground = 
-        props.previousBackground;
+        this.previousBackground = props.previousBackground;
         this.className = props.className;
         this.favSubjects = props.favSubjects;
     }
     
-    listSubjects() {
+    listsSubjects() {
         for (let i = 0; i < this.favSubjects.length; i++) {
             console.log(`${this.name} <3's ${this.favSubjects[i]}`);
         }
@@ -63,11 +61,54 @@ this.favInstructor = props.favInstructor;
 standup(channel) {
     console.log(`${this.name} announces to ${channel}, @channel standy times!`);
 }
-}
 
 debugsCode(student, subject) {
-    console.log(`${this.name} debugs ${studen.name}'s code on ${subject}`);
-}
+    console.log(`${this.name} debugs ${student.name}'s code on ${subject}`);
+ }
 }
 
-consgt fred = 
+const tony = new Instructor({
+    name: 'Tony',
+    location: 'Malibu',
+    age: 45,
+    gender: 'alpha-male',
+    favLanguage: 'JavaScript',
+    specialty: 'Annoying AI',
+    catchPhrase: 'Too bad'
+});
+
+const jarvis = new ProjectManager({
+    name: 'Jarvis',
+    location: 'Malibu',
+    age: 1, 
+    gender: 'neutral',
+    gradClassName: 'Flight',
+    favLanguage: 'JavaScript',
+    specialty: "Patience",
+    catchPhrase: 'Sir, that is not possible!'
+});
+
+const pepper = new Student({
+    name: 'Pepper',
+    location: 'Los Angeles',
+    age: 31,
+    gender: 'female',
+    previousBackground: 'School Secretary',
+    className: 'Debugging Tony',
+    favSubjects: ['Fixing Problems', 'JavaScript', 'Scolding Tony']
+});
+
+tony.speak();
+tony.demo('flying');
+tony.grade(pepper, 'Fixing Problems');
+
+pepper.speak();
+pepper.listsSubjects();
+pepper.PRAssignment('Resigning');
+pepper.sprintChallenge('Remaking Tony');
+
+jarvis.speak();
+jarvis.demo('Flying');
+jarvis.grade(pepper, 'Putting Tony in his place');
+jarvis.debugsCode(pepper, 'Putting Tony in his place');
+jarvis.standup('#Female-Allstars');
--- assignments/prototype-refactor.js
+++ assignments/prototype-refactor.js
@@ -2,8 +2,101 @@
 
 Prototype Refactor
 
-1. Copy and paste your code or the solution from yesterday
+1. Copy and paste your code or the solution from yesterday X
 
 2. Your goal is to refactor all of this code to use ES6 Classes. The console.log() statements should still return what is expected of them.
 
 */
+class GameObject {
+constructor(options) {
+    this.createdAt = options.createdAt;
+    this.dimensions = options.dimensions;
+  }
+ destroy () {
+    return `Object was removed from the game.`;
+  }
+}
+
+  class CharacterStats extends GameObject {
+  constructor(characterStatsOptions) {
+super(characterStatsOptions); 
+this.hp = characterStatsOptions.hp;
+this.name = characterStatsOptions.name; 
+  }
+takeDamage () {
+    return `${this.name} took damage.`;
+  }
+}
+
+   class Humanoid extends CharacterStats {
+     constructor (humanoidOptions) {
+      super(humanoidOptions);
+      this.cohort = humanoidOptions.cohort;
+     this.weapons = humanoidOptions.weapons;
+     this.language = humanoidOptions.language;
+   }
+   greet() {
+     return `${this.name} offers a greeting in ${this.language}.`;
+   }
+}
+
+    const mage = new Humanoid({
+      createdAt: new Date(),
+      dimensions: {
+        length: 2,
+        width: 1,
+        height: 1,
+      },
+      hp: 5,
+      name: 'Bruce',
+      cohort: 'Mage Guild',
+      weapons: [
+        'Staff of Shamalama',
+      ],
+      language: 'Common Russian Tongue',
+    });
+  
+    const swordsman = new Humanoid({
+      createdAt: new Date(),
+      dimensions: {
+        length: 2,
+        width: 2,
+        height: 2,
+      },
+      hp: 15,
+      name: 'Sir Mustachio',
+      cohort: 'The Round Table',
+      weapons: [
+        'Giant Sword',
+        'Shield',
+      ],
+      language: 'Common Mongolian Tongue',
+    });
+  
+    const archer = new Humanoid({
+      createdAt: new Date(),
+      dimensions: {
+        length: 1,
+        width: 2,
+        height: 4,
+      },
+      hp: 10,
+      name: 'Lilith',
+      cohort: 'Forest Kingdom',
+      weapons: [
+        'Bow',
+        'Dagger',
+      ],
+      language: 'Elvish',
+    });
+  
+    console.log(mage.createdAt); 
+    console.log(archer.dimensions); 
+    console.log(swordsman.hp); 
+    console.log(mage.name); 
+    console.log(swordsman.cohort); 
+    console.log(mage.weapons);
+    console.log(archer.language); 
+    console.log(archer.greet()); 
+    console.log(mage.takeDamage()); 
+    console.log(swordsman.destroy()); 
